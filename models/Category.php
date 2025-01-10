cd40dbbadc92541732823f89403fe5934edfd4e8
medlaq777 <m.laqsoumi@gmail.com>
2025-01-10 15:01:56 +0100

    fix issues delete crud operations for all users and also create task or projects

--- models/Category.php
+++ models/Category.php
@@ -30,14 +30,14 @@ class Category {
         $query = "SELECT * FROM categories";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
     public function getById($id) {
         $query = "SELECT * FROM categories WHERE id = ?";
         $stmt = $this->db->prepare($query);
         $stmt->execute([$id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 }
 
